c2b392a1a15652437e2a0b9ebf71d9ab90903ffb
Michael Dinata <michaeldinata@Michaels-MacBook-Pro.local>
2020-09-22 01:02:24 +0800

    Added lambdas function

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -7,6 +7,7 @@ import se.edu.inclass.task.TaskNameComparator;
 
 import java.util.ArrayList;
 import java.util.stream.Collectors;
+import static java.util.stream.Collectors.toList;
 
 public class Main {
 
@@ -20,6 +21,12 @@ public class Main {
         printDeadlines(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+
+        printDeadlinesUsingStreams(tasksData);
+
+        for(Task t: filterByString(tasksData, "11")){
+            System.out.println(t);
+        }
 
     }
 
@@ -45,5 +52,20 @@ public class Main {
                 System.out.println(t);
             }
         }
+    }
+
+    public static void printDeadlinesUsingStreams(ArrayList<Task> tasksData){
+        tasksData.stream()
+                .filter((s) -> s instanceof Deadline)
+                .sorted((a, b) -> a.getDescription().toLowerCase().compareTo(b.getDescription().toLowerCase()))
+                .forEach(System.out::println);
+    }
+
+    public static ArrayList<Task> filterByString(ArrayList<Task> tasksData, String filterString){
+        ArrayList<Task> filteredTaskList = (ArrayList<Task>) tasksData.stream()
+                .filter((s) -> s.getDescription().contains(filterString))
+                .collect(toList());
+        
+        return filteredTaskList;
     }
 }
